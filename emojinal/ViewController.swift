afee26972bb50a1e70d7e3366346be4419f301b5
Ipsita Bisht <ipsitabisht@gmail.com>
2020-07-21 17:06:07 -0700

    Complete iteration 2

--- emojinal/ViewController.swift
+++ emojinal/ViewController.swift
@@ -9,14 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let emojis = ["😍": "Heart eyes", "🥺": "Puppy eyes"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
     @IBAction func showMessage(sender: UIButton) {
-        let alertController = UIAlertController(title: "Emojinal", message: "It's ok" , preferredStyle: UIAlertController.Style.alert)
+        // "?" : This is a form of unwrapping. Meaning “please check if the sender does indeed have a titleLabel property; if it does, please continue.”
+        // gets the specifc emoji from the button
+        let selectedEmotion = (sender.titleLabel?.text)!
+        
+        let alertController = UIAlertController(title: "Emojinal", message: emojis[selectedEmotion]!, preferredStyle: UIAlertController.Style.alert)
        
         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
         present(alertController, animated: true, completion: nil)
